a0e240877b009de8f2f648bd62e8f7950039b451
agustin salazar <mario.agustin.salazar@gmail.com>
2023-09-02 21:48:22 -0300

    correcion de models/itineraries

--- src/middlewares/itineVerify.js
+++ src/middlewares/itineVerify.js
@@ -1,13 +1,22 @@
 const itineraryDataVerification = (req, res, next) => {
-    const { name, like, price, duration } = req.body;
+    const { name, user, like, image, price, duration } = req.body;
 
     if (!name || typeof name !== "string") {
         return res.status(400).json({ message: "Invalid name" });
     }
 
-    if (like !== undefined && typeof like !== "string" && typeof like !== "number") {
+    if (!user|| typeof user !== "string") {
+        return res.status(400).json({ message: "Invalid user" });
+    }
+
+    if (!image || typeof image !== "string") {
+        return res.status(400).json({ message: "Invalid image" });
+    }
+
+    if (like !== undefined && !Array.isArray(like)) {
         return res.status(400).json({ message: "Invalid like" });
     }
+    
 
     if (!price || typeof price !== "number") {
         return res.status(400).json({ message: "Invalid price" });
--- src/models/itinerary.js
+++ src/models/itinerary.js
@@ -2,12 +2,14 @@ const { Schema, model } = require('mongoose');
 
 let collection = "itineraries";
 let schema = new Schema({
+    user: { type: String, required: true},
+    image: { type: String, required: true},
     name: { type: String, required: true },
     city_id: { type: Schema.Types.ObjectId, required: true, ref: 'city' }, 
     price: { type: Number, required: true },
     duration: { type: Number, required: true },
     tags: [{ type: String, required: true }],
-    like: { type: String, required: true },
+    like: [{ type: String, required: true }],
 }, {
     timestamps: true
 });
