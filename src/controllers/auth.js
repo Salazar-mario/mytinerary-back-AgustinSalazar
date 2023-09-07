0eb8c061dbe37be0fc4ea7f1efe5cdcbd01dcced
agustin salazar <mario.agustin.salazar@gmail.com>
2023-09-07 19:27:32 -0300

    se modifica auth/jwt

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,22 +1,70 @@
-const User = require('../models/user');
+import User from "../models/User.js"
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-const register = async (req, res) => {
-    try {
-        const payload = req.body;
-        const userExists = await User.findOne({ email: payload.email });
-        if (userExists) {
-            return res.status(403).json({ message: "The user already exists" });
+const authController = {
+
+    signUp: async (req, res, next) => {
+        try {
+            const passwordHash = bcrypt.hashSync(req.body.password, 10)
+
+            let body = { ...req.body }
+            body.password = passwordHash
+
+            const newUser = await User.create(body)
+            let { _id, name, email, profile_pic } = newUser
+            const token = jwt.sign( { _id, name }, process.env.SECRET_KEY, { expiresIn:'1h' } )
+            return res.status(201).json({
+                success: true,
+                userData:{ name, email, profile_pic },
+                token: token,
+                message: 'Sign up successfully'
+            })
+
+        } catch (error) {
+            console.log(error);
+            next(error)
         }
-        const userCreated = await User.create(payload);
+    },
+
+    signIn: async (req, res, next) => {
+        try {
+            let { email, password } = req.body
+            const userExists = await User.findOne({ email })
+            if (userExists) {
+                const passwordValid = bcrypt.compareSync(password, userExists.password)
+                if (passwordValid){
+                    let { _id, name, email, profile_pic } = userExists
+                    const token = jwt.sign( { _id, name }, process.env.SECRET_KEY, { expiresIn:'1h' } )
+                    return res.status(201).json({
+                        success: true,
+                        userData: { name, email, profile_pic },
+                        token: token,
+                        message: 'Sign in successfully'
+                    })
+                }
+            }
+            return res.status(400).json({
+                success: false,
+                message: 'User / Password incorrect'
+            })
+        } catch (error) {
+            console.log(error);
+            next(error) 
+        }
+    },
+
+    loginWithToken : (req, res) => {
+        const { name, email, profile_pic } = req.user
         res.status(200).json({
-            message: "User created successfully",
-            userCreated
-        });
-    } catch (e) {
-        res.status(400).json({ message: e.message });
+            success: true,
+            userData: {  name, email, profile_pic },
+            message: 'Sign in with token successfully',
+        })
     }
-};
+
+}
 
 module.exports = {
-    register
-};
+authController
+}
